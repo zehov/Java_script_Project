65da0111290981c853c4bf1d29b093825111e6fa
zehov <zehov@abv.bg>
2017-06-20 22:05:30 +0300

    begining
    start project

---
+++ caesar-cipher.js
@@ -0,0 +1,68 @@
+/* 
+ * Caesar cipher
+ * 
+ * Copyright (c) 2017 Project Nayuki
+ * All rights reserved. Contact Nayuki for licensing.
+ * https://www.nayuki.io/page/caesar-cipher-javascript
+ */
+
+//"use strict";
+
+
+/* 
+ * Handles the HTML input/output for Caesar cipher encryption/decryption.
+ * This is the one and only entry point function called from the HTML code.
+ */
+function doCrypt(isDecrypt) {
+	var shiftText = document.getElementById("shift").value;
+	if (!/^-?\d+$/.test(shiftText)) {
+		alert("Shift is not an integer");
+		return;
+	}
+	var shift = parseInt(shiftText, 10);
+	if (shift < 0 || shift >= 26) {
+		alert("Shift is out of range");
+		return;
+	}
+	if (isDecrypt)
+		shift = (26 - shift) % 26;
+	var textElem = document.getElementById("text");
+	textElem.value = caesarShift(textElem.value, shift);
+}
+
+
+/* 
+ * Returns the result of having each alphabetic letter of the given text string shifted forward
+ * by the given amount, with wraparound. Case is preserved, and non-letters are unchanged.
+ * Examples:
+ * - caesarShift("abz",  0) = "abz".
+ * - caesarShift("abz",  1) = "bca".
+ * - caesarShift("abz", 25) = "zay".
+ * - caesarShift("THe 123 !@#$", 13) = "GUr 123 !@#$".
+ */
+function caesarShift(text, shift) {
+	var result = "";
+	for (var i = 0; i < text.length; i++) {
+		var c = text.charCodeAt(i);
+		if      (65 <= c && c <=  90) result += String.fromCharCode((c - 65 + shift) % 26 + 65);  // Uppercase
+		else if (97 <= c && c <= 122) result += String.fromCharCode((c - 97 + shift) % 26 + 97);  // Lowercase
+		else                          result += text.charAt(i);  // Copy
+	}
+	return result;
+}
+console.log(caesarShift("Здравей!",2));
+console.log((26-6)%26);
+// in BG
+function caesarShift(text, shift) {
+	var result = "";
+	for (var i = 0; i < text.length; i++) {
+		var c = text.charCodeAt(i);
+		if      (1040 <= c && c <=  1071) result += String.fromCharCode((c - 1040 + shift) % 32 + 1040);  // Uppercase
+		else if (1072 <= c && c <= 1103) result += String.fromCharCode((c - 1072 + shift) % 32 + 1072);  // Lowercase
+		else                          result += text.charAt(i);  // Copy
+	}
+	return result;
+}
+console.log(caesarShift("Йжтвдзл!",-2));
+
+console.log((32-6)%32);
