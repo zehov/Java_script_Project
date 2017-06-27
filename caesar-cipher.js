cd1e583b27beefe7bfac909fce3fd3d7f2e910b0
zehov <zehov@abv.bg>
2017-06-27 18:52:25 +0300

    stage two
    free html templete

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -1,18 +1,6 @@
-/* 
- * Caesar cipher
- * 
- * Copyright (c) 2017 Project Nayuki
- * All rights reserved. Contact Nayuki for licensing.
- * https://www.nayuki.io/page/caesar-cipher-javascript
- */
-
-//"use strict";
 
 
-/* 
- * Handles the HTML input/output for Caesar cipher encryption/decryption.
- * This is the one and only entry point function called from the HTML code.
- */
+ 
 function doCrypt(isDecrypt) {
 	var shiftText = document.getElementById("shift").value;
 	if (!/^-?\d+$/.test(shiftText)) {
@@ -31,15 +19,8 @@ function doCrypt(isDecrypt) {
 }
 
 
-/* 
- * Returns the result of having each alphabetic letter of the given text string shifted forward
- * by the given amount, with wraparound. Case is preserved, and non-letters are unchanged.
- * Examples:
- * - caesarShift("abz",  0) = "abz".
- * - caesarShift("abz",  1) = "bca".
- * - caesarShift("abz", 25) = "zay".
- * - caesarShift("THe 123 !@#$", 13) = "GUr 123 !@#$".
- */
+
+ 
 function caesarShift(text, shift) {
 	var result = "";
 	for (var i = 0; i < text.length; i++) {
